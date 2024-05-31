840a51ba15bcbfae8e951db778d1b8aca6157e51
Krishnachandarana11 <158564518+Krishnachandarana11@users.noreply.github.com>
2024-05-31 14:01:10 +0530

    Update task5.dart

--- Project1 Quick Starter/task5.dart
+++ Project1 Quick Starter/task5.dart
@@ -3,19 +3,19 @@ import 'dart:io';
 void main() {
   List<List<int>> Matrix = 
   [
-    [3, 2, 1],
+    [9, 8, 7],
     [4, 5, 6],
-    [9, 8, 7]
+    [3, 2, 1]
   ];
 
   while (true) 
   {
     print("\nMenu:");
-    print("1. Sum Of all element");
-    print("2. Sum Of Specific Row");
-    print("3. Sum Of Specific Column");
-    print("4. Sum Of Siagonal Elements");
-    print("0. Exit");
+    print("Press 1 Sum Of all element");
+    print("Press 2 Sum Of Specific Row");
+    print("Press 3 Sum Of Specific Column");
+    print("Press 4 Sum Of Diagonal Elements");
+    print("Press 0 Exit");
 
     stdout.write("Enter your choice: ");
     int choice = int.parse(stdin.readLineSync()!);
@@ -29,41 +29,41 @@ void main() {
         {
           for (int j = 0; j < Matrix[i].length; j++) 
           {
-            sum += Matrix[i][j];
+            sum+=Matrix[i][j];
           }
         }
-        print("Sum of all elements: $sum");
+        print("Sum Of Element: $sum");
         break;
 
       case 2:
-        stdout.write("Enter row number (0-2): ");
+        stdout.write("Enter Row Numbers : ");
         int row = int.parse(stdin.readLineSync()!);
         int sum = 0;
         for (int i = 0; i < Matrix[row].length; i++) 
         {
-          sum += Matrix[row][i];
+          sum+=Matrix[row][i];
         }
-        print("Sum of row $row: $sum");
+        print("Sum Of Row : $sum");
         break;
 
       case 3:
-        stdout.write("Enter column number (0-2): ");
+        stdout.write("Enter Column Number : ");
         int column = int.parse(stdin.readLineSync()!);
         int sum = 0;
         for (int i = 0; i < Matrix.length; i++) 
         {
-          sum += Matrix[i][column];
+          sum+=Matrix[i][column];
         }
-        print("Sum of column $column: $sum");
+        print("Sum Of Column : $sum");
         break;
 
       case 4:
         int sum = 0;
         for (int i = 0; i < Matrix.length; i++) 
         {
-          sum += Matrix[i][i];
+          sum+=Matrix[i][i];
         }
-        print("Sum of diagonal elements: $sum");
+        print("Sum Of Diagonal Elements: $sum");
         break;
 
         case 0:
@@ -73,7 +73,11 @@ void main() {
         }
 
       default:
-        print("Invalid choice!");
+        print("Invalid Choice");
     }
   }
 }
+
+     
+       
+       
