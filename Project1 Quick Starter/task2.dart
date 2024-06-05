90e65b8df96e46b1b4d7c3904ab05b8f681b701d
Krishnachandarana11 <158564518+Krishnachandarana11@users.noreply.github.com>
2024-06-05 14:06:34 +0530

    Update task2.dart

--- Project1 Quick Starter/task2.dart
+++ Project1 Quick Starter/task2.dart
@@ -1,4 +1,16 @@
 void main()
 {
-    
+    List<int> number =[1,2,3,4,5,6,7,8,9,10];
+
+  int largestnumber = number[0];
+
+   for(int i=0;i<number.length;i++)
+    {
+        if(number[i]>largestnumber)
+        {
+           largestnumber = number[i];
+        }
+    }  
+
+    print("$largestnumber");
 }
